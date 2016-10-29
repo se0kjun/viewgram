a2e1ef7aa1a917fbbd4e56f5c2643a739ab2fe7b
SeokJun Hong <hong921122@gmail.com>
2016-10-30 00:41:18 +0900

    add viewing 360 degrees video

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 'use strict';
 
+AFRAME.registerComponent('not-fullscreen', {
+    init: function() {
+        document.querySelector('video#video_data').pause();
+    },
+    tick: function () {
+        var canvasEl = this.el.querySelector('canvas.a-canvas');
+        canvasEl.setAttribute('style', 'width:100%;height:700px;');
+    }
+});
+
 window.addEventListener("load", function () {
     var svg_config = {
         'width': 200,
-        'height': 200
+        'height': 200,
+        'axis_margin': 50
     };
     var rect_config = {
         'width': 5,
@@ -25,9 +36,12 @@ window.addEventListener("load", function () {
         color_level = 5,
         frame_level = 0,
         whole_frame_count;
+//        line_axis = d3.svg.line()
+//        .x(function(d){return d.x})
+//        .y(function(d){return d.y});
     
     var svg_init = (function() {
-        svg_config.height = rect_config.height * (360 / angle_sample);
+        svg_config.height = rect_config.height * (360 / angle_sample) + svg_config.axis_margin;
         
         svg = d3.select('#viewgram')
         .append('svg')
@@ -52,28 +66,37 @@ window.addEventListener("load", function () {
                 });
             });
             
+            var max_angle_data_lookup = angle_data_lookup.reduce(function(previous_d, current_d, idx, arr) {
+                return (current_d > arr[previous_d]) ? idx : previous_d;
+            }, 0);
+                
             // append g of wrapping rects
             var g_wrapper = svg.append('g')
             .attr('id', 'frame_' + i.toString())
             .attr('data-frame', i.toString())
-            .attr('transform', 'translate(' + (rect_config.width * i).toString() + ')');
+            .attr('transform', 'translate(' + (rect_config.width * i + svg_config.axis_margin).toString() + ')');
 
             // append rect
             angle_data_lookup.forEach(function(d, idx, arr) {
-                g_wrapper.append('rect')
-                .attr('width', rect_config.width)
-                .attr('height', rect_config.height)
-                .attr('y', rect_config.height * idx)
-                .attr('data-count', d)
-                .attr('data-detail', arr)
-                .style('fill', colorPick(color_level_count, d))
-                .on('mouseover', showCount)
-                .on('mouseleave', hideCount)
-                .on('click', showInfo);
+                if (idx < 360 / angle_sample)
+                    g_wrapper.append('rect')
+                    .attr('width', rect_config.width)
+                    .attr('height', rect_config.height)
+                    .attr('y', rect_config.height * idx)
+                    .attr('data-count', d)
+                    .attr('data-detail', arr)
+                    .attr('data-angle', idx * angle_sample)
+                    .style('fill', colorPick(color_level_count, d))
+                    .style('stroke', (idx == max_angle_data_lookup) ? '#FFF' : 'none')
+                    .on('mouseover', showCount)
+                    .on('mouseleave', hideCount)
+                    .on('click', showInfo);
             });
         }
         
         createColorLabel();
+//        createYAxis();
+//        createXAxis();
     });
     
     function colorPick(sample, angle_data) {
@@ -86,9 +109,11 @@ window.addEventListener("load", function () {
     function videoPreview(click_node) {
         var videoData = document.getElementById('video_data');
         var count_data = click_node.parentElement.getAttribute('data-frame');
+        var angle_data = click_node.getAttribute('data-angle');
         
         // set frame data
         videoData.currentTime = (count_data * frame_sample) / 60;
+        document.querySelector('a-videosphere').setAttribute('rotation', '0 ' + angle_data.toString() + ' 0');
     }
     
     function showCount() {
@@ -105,13 +130,32 @@ window.addEventListener("load", function () {
         videoPreview(this);
     }
     
-    function createAxis() {
+    function createYAxis() {
+        var axis = svg.append('g')
+        .attr('transform', 'translate(40)');
+        
+        axis.append('path')
+        .attr('d', line_axis(lineData))
+        .attr('stroke', 'white')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
+    }
+    
+    function createXAxis() {
+        var axis = svg.append('g')
+        .attr('transform', 'translate(40)');
+        
+        axis.append('path')
+        .attr('d', line_axis(lineData))
+        .attr('stroke', 'white')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
     }
     
     function createColorLabel() {
         var color_label_wrapper = svg.append('g')
         .attr('id', 'color-label')
-        .attr('transform', 'translate(' + ((whole_frame_count / frame_sample) * rect_config.width + 20).toString() + ')');
+        .attr('transform', 'translate(' + ((whole_frame_count / frame_sample) * rect_config.width + 20 + svg_config.axis_margin).toString() + ')');
         
         rect_config.color.forEach(function(color, idx) {
             color_label_wrapper.append('rect')
